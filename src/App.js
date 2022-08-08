b8039049913aad939385a71dd19333793ea5d59f
Kasra <Kasraebr1378@gmail.comgit config --listclear>
2022-08-08 19:04:39 -0400

    6 commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import AddContact from "./Add Contact/AddContact.js";
 import ContactList from "./ContactList";
 import "./App.css"
@@ -24,6 +24,15 @@ const deleteHandler = (id) => {
    const filteredContact = contacts.filter( c => c.id !== id);
    setContacts(filteredContact);
 }
+
+  useEffect(()=>{
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (savedContacts) setContacts(savedContacts);
+  },[]);
+  
+  useEffect(()=>{
+    localStorage.setItem("contacts", JSON.stringify(contacts) )
+  },[contacts])
   return (
     <div >
       <h1> Contact App </h1>
